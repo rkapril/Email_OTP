0890f183bc570d0125656b7d76b0393ae8d06edc
Goh Rui Kun <61505106+rkapril@users.noreply.github.com>
2022-10-10 02:07:05 +0800

    Update Node.js

--- Node.js
+++ Node.js
@@ -4,9 +4,9 @@ const msg = require("../OTP.js");
 let OTP = msg.print();
 
 const mailjet = new Mailjet({
-  apiKey: process.env.MJ_APIKEY_PUBLIC || "0b0a22319d8e3f1de10c55ef3de92667",
+  apiKey: process.env.MJ_APIKEY_PUBLIC || "your-api-key",
   apiSecret:
-    process.env.MJ_APIKEY_PRIVATE || "20533d7c968b40b803ee55e9e332ef19",
+    process.env.MJ_APIKEY_PRIVATE || "your-api-secret",
 });
 
 const request = mailjet.post("send", { version: "v3.1" }).request({
